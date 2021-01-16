11c391cd1425effd516716dc888cb50e7656b5fd
yudisthirogithub <yudis_deon@yahoo.com>
2021-01-16 17:08:49 +0700

    admin page add latihan keyboards stuffs done

--- ARChemy/Admin/NewLatihanViewController.swift
+++ ARChemy/Admin/NewLatihanViewController.swift
@@ -27,7 +27,16 @@ class NewLatihanViewController: UIViewController {
     var judul = ""
     
     override func viewDidLoad() {
+        
+        // test code
+        initializeHideKeyboard()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        // test code
+        
         changeBorderToGreen()
+        changeBorderToRed()
         super.viewDidLoad()
         initializeHideKeyboard()
         database = Database.database().reference()
@@ -118,5 +127,26 @@ extension NewLatihanViewController {
           w1TextField.layer.borderColor = myColor.cgColor
           w2TextField.layer.borderColor = myColor.cgColor
           w3TextField.layer.borderColor = myColor.cgColor
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+}
+
+extension NewLatihanViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
