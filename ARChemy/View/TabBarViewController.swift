88b84c23f78951b26267e21aa83fbb72037b0d41
yudisthirogithub <yudis_deon@yahoo.com>
2021-01-16 15:28:56 +0700

    tab bar edited

--- ARChemy/View/TabBarViewController.swift
+++ ARChemy/View/TabBarViewController.swift
@@ -16,9 +16,14 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ThreeMainBar.selectedItem?.image = UIImage(named: "selectedModeAR")
-        
         self.selectedIndex = idx
+        
+        if(self.selectedIndex == idx){
+//            ThreeMainBar.selectedItem?.image = UIImage(named: "selectedModeAR")
+            ThreeMainBar.backgroundColor = .white
+            ThreeMainBar.tintColor  = UIColor(red: 141/255, green: 139/255, blue: 237/255, alpha: 1)
+            
+        }
     }
 
     func check() {
