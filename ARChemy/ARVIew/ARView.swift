30f4c9728c75bfee607df5a7bc442b963fe4021b
Vincent Alexander Christian <jvincentalexnder@gmail.com>
2021-01-20 23:31:13 +0700

    masih ada bug di arview

--- ARChemy/ARVIew/ARView.swift
+++ ARChemy/ARVIew/ARView.swift
@@ -16,8 +16,13 @@ class ARView: UIViewController, ARSCNViewDelegate {
     var symbol = ""
     var elementCount = InitViewController.arrayOfElements.count
     
+    var helperArr: [SCNNode] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        sceneView.addGestureRecognizer(tapGesture)
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -32,7 +37,6 @@ class ARView: UIViewController, ARSCNViewDelegate {
         sceneView.scene = scene
         
         configureAR()
-//        addLine(x: 0, y: 0, z: -1.5)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -80,6 +84,22 @@ class ARView: UIViewController, ARSCNViewDelegate {
 }
 
 extension ARView {
+    @objc func handleTap(_ gestureRecognize: UITapGestureRecognizer) {
+        let location = gestureRecognize.location(in: sceneView)
+        let hitResults = sceneView.hitTest(location, options: [:])
+        
+        if hitResults.count > 0 {
+            let tappedPiece = hitResults[0].node
+            
+            for helper in helperArr {
+                if helper.parent == tappedPiece {
+                    helper.isHidden.toggle()
+                    break
+                }
+            }
+        }
+    }
+    
     func configureAR() {
         if elementCount == 1 {
             symbol = InitViewController.arrayOfElements[elementCount-1].symbol
@@ -87,11 +107,9 @@ extension ARView {
         }
         else if elementCount == 2 {
             symbol = InitViewController.arrayOfElements[0].symbol
-            addSphere(x: -0.2, y: 0, z: -1.5, symbol: symbol, color: UIColor.orange)
+            addSphere(x: -0.25, y: 0, z: -1.5, symbol: symbol, color: UIColor.orange)
             symbol = InitViewController.arrayOfElements[1].symbol
-            addSphere(x: 0.2, y: 0, z: -1.5, symbol: symbol, color: UIColor.blue)
-            
-            
+            addSphere(x: 0.25, y: 0, z: -1.5, symbol: symbol, color: UIColor.blue)
         }
         else if elementCount == 3 {
             symbol = InitViewController.arrayOfElements[0].symbol
@@ -100,8 +118,6 @@ extension ARView {
             addSphere(x: 0.25, y: -0.25, z: -1.5, symbol: symbol, color: UIColor.blue)
             symbol = InitViewController.arrayOfElements[2].symbol
             addSphere(x: -0.25, y: -0.25, z: -1.5, symbol: symbol, color: UIColor.red)
-            
-            
         }
         else if elementCount == 4 {
             symbol = InitViewController.arrayOfElements[0].symbol
@@ -112,8 +128,6 @@ extension ARView {
             addSphere(x: -0.25, y: -0.25, z: -1.5, symbol: symbol, color: UIColor.red)
             symbol = InitViewController.arrayOfElements[3].symbol
             addSphere(x: -0.25, y: 0.25, z: -1.5, symbol: symbol, color: UIColor.yellow)
-            
-            
         }
         else if elementCount == 5 {
             symbol = InitViewController.arrayOfElements[0].symbol
@@ -139,21 +153,61 @@ extension ARView {
         label.text = symbol
         label.textColor = .white
         
+        let electronView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        electronView.backgroundColor = .lightGray
+        let electronLabel = UILabel(frame: CGRect(x: 0, y: 35, width: 200, height: 30))
+        electronLabel.font = UIFont.systemFont(ofSize: 30)
+        electronView.addSubview(electronLabel)
+        electronLabel.textAlignment = .center
+        electronLabel.text = "-"
+        electronLabel.font = .systemFont(ofSize: 80)
+        electronLabel.textColor = .black
+        
         let sphere = SCNSphere(radius: 0.1)
         sphere.firstMaterial?.diffuse.contents = view
         
+        let action = SCNAction.rotateBy(x: 0, y: CGFloat(2 * Double.pi), z: 0, duration: 7)
+        let repAction = SCNAction.repeatForever(action)
+        
+        let helperAction = SCNAction.rotateBy(x: 0, y: 0, z: 0, duration: 8)
+        let repHelperAction = SCNAction.repeatForever(helperAction)
+        
         let sphereNode = SCNNode(geometry: sphere)
         sphereNode.position = SCNVector3(x, y, z)
+        sphereNode.runAction(repAction)
+        
+        let helperNode = SCNNode()
+        helperNode.position = SCNVector3(0,0,0)
+        helperArr.append(helperNode)
+        
+        var initX: CGFloat = 0
+        var initZ: CGFloat = 0
+        
+        for node in InitViewController.arrayOfElements {
+            let angle: CGFloat = 360 / CGFloat(node.electrons)
+            var initAngle = angle
+
+            for _ in 0..<node.electrons {
+                let moon = SCNSphere(radius: 0.02)
+                moon.firstMaterial?.diffuse.contents = electronView
+
+                let moonNode = SCNNode(geometry: moon)
+
+                initX = 0.3 * cos(initAngle)
+                initZ = 0.3 * sin(initAngle)
+
+                moonNode.position = SCNVector3(initX,0,initZ)
+                helperNode.addChildNode(moonNode)
+
+                initAngle += angle
+            }
+        }
+        
+        sphereNode.addChildNode(helperNode)
+        
+        helperNode.runAction(repHelperAction)
+        helperNode.isHidden = true
+        
         sceneView.scene.rootNode.addChildNode(sphereNode)
-    }
-    
-    func addLine(x: CGFloat, y: CGFloat, z: CGFloat) {
-        let line = SCNCylinder(radius: 0.05, height: 0.2)
-        line.firstMaterial?.diffuse.contents = UIColor.black
-        
-        let lineNode = SCNNode(geometry: line)
-        lineNode.position = SCNVector3(x, y, z)
-        
-        sceneView.scene.rootNode.addChildNode(lineNode)
     }
 }
--- ARChemy/Admin/LoginViewController.swift
+++ ARChemy/Admin/LoginViewController.swift
@@ -35,32 +35,36 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginBtn(_ sender: Any) {
-        login(name: nameTextField.text!, password: passwordTextField.text!)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [self] in
-            if self.isLogin == true{
-                print("login success")
-                
-                let sb = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AdminHome") as! AdminHomeViewController
-                sb.modalPresentationStyle = .fullScreen
-                sb.teacher = self.teacher
-                sb.teacherName = nameTextField.text!
-
-                self.present(sb, animated: true, completion: nil)
+        if nameTextField.text != "" && passwordTextField.text != "" {
+            login(name: nameTextField.text!, password: passwordTextField.text!)
             
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [self] in
+                if self.isLogin == true{
+                    print("login success")
+                    
+                    let sb = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AdminHome") as! AdminHomeViewController
+                    sb.modalPresentationStyle = .fullScreen
+                    sb.teacher = self.teacher
+                    sb.teacherName = nameTextField.text!
+
+                    self.present(sb, animated: true, completion: nil)
+                
+                }
+                else {
+                    makeAlert(title: "Gagal", desc: "Nama atau Password salah")
+                    nameTextField.endEditing(true)
+                    passwordTextField.endEditing(true)
+                }
             }
-            else {
-                makeAlert(title: "Gagal", desc: "Nama atau Password salah")
-                nameTextField.endEditing(true)
-                passwordTextField.endEditing(true)
-            }
+        }
+        else {
+            makeAlert(title: "Perhatian", desc: "Mohon Isi Nama dan Password")
         }
     }
     
     @IBAction func signUpBtn(_ sender: Any) {
         if nameTextField.text != "" && passwordTextField.text != "" {
             addNewTeacher(name: nameTextField.text!, password: passwordTextField.text!)
-            
             makeAlert(title: self.warningTitle, desc: self.warningDesc)
         }
         else {
